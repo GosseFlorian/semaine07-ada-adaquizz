4ce1e96b97a99da84c43ad79325151b547a47815
GosseFlorian <floriangosse91@gmail.com>
2026-03-11 13:15:03 +0100

    structuration en fichier js pour chaque action

--- src/main.js
+++ src/main.js
@@ -1,36 +1,39 @@
 import './style.css'
 import quiz from "./quiz-femmes-scientifiques.json";
 import { startQuizz } from './start';
+import { showQuestion } from './show-question';
+import { nextQuestion } from './next-question';
+import { endGame } from './end';
+
+let score = 0
 
 document.querySelector('#app').innerHTML = `
     <div id="acceuil">
         <h1>${quiz.title}</h1>
         <button id="start">Démarrer</button>
-    </div>
-    <div id="screenQuizz">
-        <header>
-            <h1>${quiz.title}</h1>
-        </header>
-        <div id="divQuizz>
-            <h2 id="question"></h2>
-            <div id ="responseChoice">
-                <button id="choice1"></button>
-                <button id="choice2"></button>
-                <button id="choice3"></button>
-                <button id="choice4"></button>
-            </div>
-            <p id="responseMessage"></p>
-            <button id="continue">Continue</button>
-        </div>
-    </div>
-    <div id="endScreen">
-        <h2 id="scoreMessage"></h2>
-        <h3 id="poucentageResponseMessage"></h3>
-        <p id="score"></p>
-        <button id="replayQuizz"></button>
-    </div>
-
-
-`
-
+    </div>`
+    
 startQuizz(document.querySelector('#start'))
+
+    // <div id="screenQuizz">
+    //     <header>
+    //         <h1>${quiz.title}</h1>
+    //     </header>
+    //     <div id="divQuizz>
+    //         <h2 id="question"></h2>
+    //         <div id ="responseChoice">
+    //             <button id="choice1"></button>
+    //             <button id="choice2"></button>
+    //             <button id="choice3"></button>
+    //             <button id="choice4"></button>
+    //         </div>
+    //         <p id="responseMessage"></p>
+    //         <button id="continue">Continue</button>
+    //     </div>
+    // </div>
+    // <div id="endScreen">
+    //     <h2 id="scoreMessage"></h2>
+    //     <h3 id="poucentageResponseMessage"></h3>
+    //     <p id="score"></p>
+    //     <button id="replayQuizz"></button>
+    // </div>
--- src/start.js
+++ src/start.js
@@ -1,8 +1,6 @@
 export function startQuizz(button){
     button.addEventListener('click',() =>{
         acceuil = document.querySelector('#acceuil')
-        screenQuizz = document.querySelector('#screenQuizz')
         acceuil.style.display = "none"
-        screenQuizz.style.display = "block"
     })
 }
