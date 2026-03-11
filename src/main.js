ce41f523bba4a18db0895f1967cf91d6189db8fc
GosseFlorian <floriangosse91@gmail.com>
2026-03-11 11:22:58 +0100

    Ajoute button qui cache l'acceuil

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import './style.css'
 import quiz from "./quiz-femmes-scientifiques.json";
+import { startQuizz } from './start';
 
 document.querySelector('#app').innerHTML = `
+  <div id="acceuil">
   <h1>${quiz.title}</h1>
+  <button id="start">Démarrer</button>
+  </div>
 `
+
+startQuizz(document.querySelector('#start'), document.querySelector('#acceuil'))
---
+++ src/start.js
@@ -0,0 +1,5 @@
+export function startQuizz(button, acceuil){
+    button.addEventListener('click',() =>{
+        acceuil.style.display = "none"
+    })
+}
