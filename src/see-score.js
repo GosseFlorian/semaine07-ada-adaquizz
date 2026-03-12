05c5675ee2256d16c7f87bc08fd846d164f86cf3
GosseFlorian <floriangosse91@gmail.com>
2026-03-12 14:24:52 +0100

    test supplémentaire pour vérifier le bon fonctionnement entre show-question et see-score

--- src/see-score.js
+++ src/see-score.js
@@ -1,3 +1,8 @@
+import './style.css'
+import quiz from "./quiz-femmes-scientifiques.json";
+import { variable } from './variable';
+
 export function seeScore(){
     console.log("on a fini")
+    console.log(`résultat: ${variable.score}/${quiz.questions.length}`)
 }
