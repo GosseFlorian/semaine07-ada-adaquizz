2d566f5e1bce5692ee3b1011b9ff7ac4647c0586
GosseFlorian <floriangosse91@gmail.com>
2026-03-12 15:18:30 +0100

    suppréssion des test réalisé qui était écrite en commentaire et rangement des lignes pour meilleurs lecture

--- src/show-question.js
+++ src/show-question.js
@@ -4,37 +4,31 @@ import { variable } from './variable';
 
 export function showQuestion(){
     document.querySelector('#app').innerHTML = `
-    <h2 id="question">${quiz.questions[variable.indexQuestion].question}</h2>
-    <div id ="responseChoice">
-    <button class="choice">${quiz.questions[variable.indexQuestion].options[0]}</button>
-    <button class="choice">${quiz.questions[variable.indexQuestion].options[1]}</button>
-    <button class="choice">${quiz.questions[variable.indexQuestion].options[2]}</button>
-    <button class="choice">${quiz.questions[variable.indexQuestion].options[3]}</button>
-    </div>
+        <h2 id="question">${quiz.questions[variable.indexQuestion].question}</h2>
+        <div id ="responseChoice">
+            <button class="choice">${quiz.questions[variable.indexQuestion].options[0]}</button>
+            <button class="choice">${quiz.questions[variable.indexQuestion].options[1]}</button>
+            <button class="choice">${quiz.questions[variable.indexQuestion].options[2]}</button>
+            <button class="choice">${quiz.questions[variable.indexQuestion].options[3]}</button>
+        </div>
     `
-
     const choices = document.querySelectorAll('.choice')
-
     choices.forEach((element, index) => {
         element.addEventListener('click', () => {
             if(index === quiz.questions[variable.indexQuestion].correctIndex){
                 variable.score ++
                 document.querySelector('#app').innerHTML += `
-                <p id="goodAnswer">C'est une bonne réponse !</p>
+                    <p id="goodAnswer">C'est une bonne réponse !</p>
                 `
-                // console.log("Bonne réponse")
-                // console.log(variable.score)
             } else {
                 document.querySelector('#app').innerHTML += `
-                <p id="badAnswer">Raté, la réponse était : ${quiz.questions[variable.indexQuestion].options[quiz.questions[variable.indexQuestion].correctIndex]}</p>
+                    <p id="badAnswer">Raté, la réponse était : ${quiz.questions[variable.indexQuestion].options[quiz.questions[variable.indexQuestion].correctIndex]}</p>
                 `
-                // console.log("mauvaise réponse")
             }
             if(variable.indexQuestion < quiz.questions.length -1){
                 variable.indexQuestion ++
-                // console.log(variable.indexQuestion)
                 document.querySelector('#app').innerHTML += `
-                <button id="nextQuestion">Continue</button>
+                    <button id="nextQuestion">Continue</button>
                 `
                 document.querySelector('#nextQuestion').addEventListener('click', async() =>{
                     const { showQuestion } = await import('./show-question');
@@ -42,7 +36,7 @@ export function showQuestion(){
                 })
             } else {
                 document.querySelector('#app').innerHTML += `
-                <button id="seeScore">Score</button>
+                    <button id="seeScore">Score</button>
                 `
                 document.querySelector('#seeScore').addEventListener('click', async() =>{
                     const { seeScore } = await import('./see-score');
