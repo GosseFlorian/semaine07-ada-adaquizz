bda987f9470ca9844aa355fe63a737e4b25707d4
GosseFlorian <floriangosse91@gmail.com>
2026-03-12 15:13:19 +0100

    suppréssion de la div ".acceuil" car pas utilise pour la sémentique

--- src/main.js
+++ src/main.js
@@ -3,9 +3,7 @@ import quiz from "./quiz-femmes-scientifiques.json";
 import { showQuestion } from './show-question';
 
 document.querySelector('#app').innerHTML = `
-    <div id="acceuil">
         <h1>${quiz.title}</h1>
         <button id="start">Démarrer</button>
-    </div>
     `
 document.querySelector('#start').addEventListener('click',showQuestion)
